48ce35d4700a3237071c23b38877701e26e9b564
eddyfr3se <chrisunder@gmx.de>
2026-04-22 15:23:18 +0200

    keyboard

--- models/character.class.js
+++ models/character.class.js
@@ -3,6 +3,7 @@ class Character extends MovableObject {
     width = 150
     height = 280;
     y = 155;
+    speed = 5;
     IMAGES_WALKING = [
         'img/2_character_pepe/2_walk/W-21.png',
         'img/2_character_pepe/2_walk/W-22.png',
@@ -11,8 +12,9 @@ class Character extends MovableObject {
         'img/2_character_pepe/2_walk/W-25.png',
         'img/2_character_pepe/2_walk/W-26.png'
     ];
+    world;
 
-  
+
 
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
@@ -23,11 +25,32 @@ class Character extends MovableObject {
 
     animate() {
 
+
         setInterval(() => {
-            let i = this.currentImage % this.IMAGES_WALKING.length;  ///lez i = 5 % 6; => 0; Rest 5
-            let path = this.IMAGES_WALKING[i];
-            this.img = this.imageCache[path];
-            this.currentImage++;
+            if (this.world.keyboard.RIGHT) {
+                this.x += this.speed;
+            }
+
+             if (this.world.keyboard.LEFT) {
+                this.x -= this.speed;
+            }
+
+
+        },1000 / 60);
+
+
+
+
+        setInterval(() => {
+
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                this.x += this.speed;
+                //walk animation 
+                let i = this.currentImage % this.IMAGES_WALKING.length;  ///lez i = 5 % 6; => 0; Rest 5
+                let path = this.IMAGES_WALKING[i];
+                this.img = this.imageCache[path];
+                this.currentImage++;
+            }
         }, 100);
     }
 
