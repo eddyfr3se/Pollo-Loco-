3349869f0426e5bf063ea37386e7ca70563ccdf6
eddyfr3se <chrisunder@gmx.de>
2026-04-23 13:32:03 +0200

    Insert background objects

--- models/character.class.js
+++ models/character.class.js
@@ -1,9 +1,9 @@
 class Character extends MovableObject {
 
-    width = 150
-    height = 280;
-    y = 155;
-    speed = 5;
+    width = 150;
+    height = 250;
+    y = 180;
+    speed = 10;
     IMAGES_WALKING = [
         'img/2_character_pepe/2_walk/W-21.png',
         'img/2_character_pepe/2_walk/W-22.png',
@@ -32,28 +32,27 @@ class Character extends MovableObject {
                 this.otherDirection = false;
             }
 
-             if (this.world.keyboard.LEFT) {
+            if (this.world.keyboard.LEFT) {
                 this.x -= this.speed;
                 this.otherDirection = true;
             }
+            this.world.camera_x = -this.x;
 
-
-        },1000 / 60);
+        }, 1000 / 60);
 
 
 
 
         setInterval(() => {
-
             if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-                this.x += this.speed;
+
                 //walk animation 
                 let i = this.currentImage % this.IMAGES_WALKING.length;  ///lez i = 5 % 6; => 0; Rest 5
                 let path = this.IMAGES_WALKING[i];
                 this.img = this.imageCache[path];
                 this.currentImage++;
             }
-        }, 100);
+        }, 80);
     }
 
     jump() {
--- models/world.class.js
+++ models/world.class.js
@@ -9,15 +9,35 @@ class World {
         new Cloud()
     ];
     backgroundObjects = [
+        new BackgroundObject('img/5_background/layers/air.png', -720),
+        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', -720),
+        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', -720),
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', -720),
+
         new BackgroundObject('img/5_background/layers/air.png', 0),
         new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
         new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0)
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0),
+        new BackgroundObject('img/5_background/layers/air.png', 720),
+        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 720),
+        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 720),
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 720),
+
+        new BackgroundObject('img/5_background/layers/air.png', 720*2),
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 720*2),
+        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 720*2),
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 720*2),
+        new BackgroundObject('img/5_background/layers/air.png', 720*3),
+        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 720*3),
+        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 720*3),
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 720*3),
+
 
     ];
     canvas;
     ctx;
     keyboard;
+    camera_x = 0;
 
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext("2d");
@@ -35,10 +55,15 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+        this.ctx.translate(this.camera_x, 0);
+
         this.addObjectsToMap(this.backgroundObjects);
+
         this.addToMap(this.character);
         this.addObjectsToMap(this.clouds);
         this.addObjectsToMap(this.enemies);
+
+        this.ctx.translate(-this.camera_x, 0);
 
 
 
