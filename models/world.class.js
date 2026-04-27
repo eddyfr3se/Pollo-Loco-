4d15d4220276e4c084e0dbee3efbef640c6a0ca5
eddyfr3se <chrisunder@gmx.de>
2026-04-27 17:36:20 +0200

    StatusBar Health Finish

--- models/world.class.js
+++ models/world.class.js
@@ -26,6 +26,7 @@ class World {
             this.level.enemies.forEach((enemy) => {
                 if (this.character.isColliding(enemy)) {
                     this.character.hit();
+                    this.statusBar.setPercentage(this.character.energy);
                 }
             });
         }, 200);
@@ -35,9 +36,16 @@ class World {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
         this.ctx.translate(this.camera_x, 0);
-
         this.addObjectsToMap(this.level.backgroundObjects);
+
+
+        this.ctx.translate(-this.camera_x, 0); //back
+        ///space for fixed object
         this.addToMap(this.statusBar);
+        this.ctx.translate(this.camera_x, 0); // forwards
+
+
+
         this.addToMap(this.character);
         this.addObjectsToMap(this.level.clouds);
         this.addObjectsToMap(this.level.enemies);
